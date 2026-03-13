d54f47d491be42bc284cd5b7ecf263e5d9dd7460
이현성 <devhyun.jungle@gmail.com>
2026-03-14 00:33:23 +0900

    [Silver IV] Title: 수 찾기, Time: 160 ms, Memory: 53868 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,15 +1,15 @@
 first_input = int(input())
 first_numbers = list(map(int, input().split()))
-num_map = {}
+num_set = set()
 
-for i in range(len(first_numbers)):
-    num_map[first_numbers[i]] = i 
+for num in first_numbers: 
+    num_set.add(num)
 
 second_input = int(input())
 second_numbers = list(map(int, input().split()))
 
 for num in second_numbers:
-    if num in num_map:
+    if num in num_set: 
         print(1)
     else:
         print(0)
