409a543e15ad201aa566cb70a75e572094f45a0b
이현성 <devhyun.jungle@gmail.com>
2026-03-16 12:59:10 +0900

    [Silver II] Title: 에디터, Time: 744 ms, Memory: 95492 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.py
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -1,77 +1,85 @@
-import sys
-input = sys.stdin.readline
+# 링크드리스트 - 에디터 (백준 실버2)
+# 문제 링크: https://www.acmicpc.net/problem/1406
+import sys 
+input = sys.stdin.readline 
 
-class Node:
+class Node: 
     def __init__(self, data, prev=None, next=None):
-        self.data = data
-        self.prev = prev
+        self.data = data 
+        self.prev = prev 
         self.next = next
 
-class LinkedList:
+class LinkedList: 
     def __init__(self):
-        self.head = None
-        self.current = None
+        self.head = None 
+        self.current = None 
 
     def append(self, data):
-        if self.head is None:
+        if self.head is None: 
             self.head = Node(data)
-            self.current = self.head
+            self.current = self.head 
             return
-
+ 
         new_node = Node(data)
         self.current.next = new_node
-        new_node.prev = self.current
-        self.current = self.current.next
+        new_node.prev = self.current 
+        self.current = self.current.next 
 
-    def cursor_append(self, data):
-        if self.current is not None:
-            if self.current == self.head:
+    def cursor_append(self, data):     
+        if self.current is not None: 
+            if self.current == self.head:       
                 new_node = Node(data)
-                new_node.next = self.current
-                self.current.prev = new_node
-                self.head = new_node
+                new_node.next = self.current 
+                self.current.prev = new_node        
+                self.head = new_node 
             else:
-                prev_node = self.current.prev
+                prev_node = self.current.prev       
                 new_node = Node(data)
-                new_node.prev = prev_node
-                new_node.next = self.current
+                new_node.prev = prev_node 
+                new_node.next = self.current 
                 prev_node.next = new_node
-                self.current.prev = new_node
+                self.current.prev = new_node 
+        
 
-    def move_left(self):
-        if self.current is not None and self.current is not self.head:
-            self.current = self.current.prev
 
+    def move_left(self): 
+        if self.current is not None and self.current is not self.head:    
+            self.current = self.current.prev        
+      
     def move_right(self):
-        if self.current is not None and self.current.next is not None:
-            self.current = self.current.next
-
+        if self.current is not None and self.current.next is not None: 
+            self.current = self.current.next 
+        
     def print_nodes_as_list(self):
         res = []
-        if self.head is not None:
-            curr = self.head
-            while curr:
+        if self.head is not None: 
+            curr = self.head 
+
+            while curr: 
                 if curr.data != "":
-                    res.append(curr.data)
-                curr = curr.next
-        return res
+                    res.append(curr.data)                
+                curr = curr.next 
+      
+        return res 
 
     def delete_cursor_left(self):
-        if self.current is not None and self.current is not self.head:
-            prev_node = self.current.prev
+        if self.current is not None and self.current is not self.head: 
+            prev_node = self.current.prev 
 
             if prev_node.prev:
-                prev_prev_node = prev_node.prev
-                prev_prev_node.next = self.current
+                # 노드가 3개 이상일때
+                prev_prev_node = prev_node.prev 
+                prev_prev_node.next = self.current 
                 self.current.prev = prev_prev_node
             else:
-                self.head = self.current
+                # 노드가 2개 일때 
+                self.head = self.current 
                 self.current.prev = None
 
 li = LinkedList()
 input_data = input().rstrip()
 
-for c in input_data:
+for c in input_data: 
     li.append(c)
 li.append("")
 
@@ -81,13 +89,16 @@ for _ in range(m):
     commands = input().split()
     command = commands[0]
 
-    if command == "P":
+    if command == "P":   
         li.cursor_append(commands[1])
-    elif command == "L":
+    elif command == "L":     
         li.move_left()
     elif command == "D":
         li.move_right()
     elif command == "B":
         li.delete_cursor_left()
 
+
+
 print("".join(li.print_nodes_as_list()))
+
